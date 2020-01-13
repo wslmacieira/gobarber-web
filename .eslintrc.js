48ab502496d89c7414c54b18b2af1ff30c6ef69e
wslmacieira <wslmacieira@DESKTOP-GJDMPV4.localdomain>
2020-01-13 19:20:42 -0300

    Aula 08 Utilizando Root Import

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,11 @@ module.exports = {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn"
   },
-    settings: { 
-    "import/resolver": { "node": { "paths": ["src"] } } },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
